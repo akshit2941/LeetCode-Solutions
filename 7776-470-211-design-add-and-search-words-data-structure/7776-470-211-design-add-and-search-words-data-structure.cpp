98feadad3bca3fb5f6a0662ec999e51fbf83704f
Akshit Saini <115421818+akshit2941@users.noreply.github.com>
2025-02-11 19:23:44 +0530

    Time: 292 ms, Memory: 563.1 MB - LeetHub

---
+++ 7776-470-211-design-add-and-search-words-data-structure/7776-470-211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,56 @@
+struct Node {
+    Node* links[26];
+    bool flag = false;
+
+    bool containsKey(char ch) { return links[ch - 'a'] != nullptr; }
+    void put(char ch, Node* node) { links[ch - 'a'] = node; }
+    Node* get(char ch) { return links[ch - 'a']; }
+    void setEnd() { flag = true; }
+    bool isEnd() { return flag; }
+};
+class WordDictionary {
+private:
+    Node* root;
+
+public:
+    WordDictionary() { root = new Node(); }
+
+    void addWord(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+
+    bool searchHelper(string& word, int index, Node* node) {
+        if (index == word.length())
+            return node->isEnd();
+        char ch = word[index];
+        if (ch == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (node->links[i] &&
+                    searchHelper(word, index + 1, node->links[i])) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            if (!node->containsKey(ch))
+                return false;
+            return searchHelper(word, index + 1, node->get(ch));
+        }
+    }
+
+    bool search(string word) { return searchHelper(word, 0, root); }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
